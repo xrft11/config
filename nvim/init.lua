48c67bc3df445aa4c9e98dfd2076429cdb840cd0
xrft11 <85630585+xrft11@users.noreply.github.com>
2021-12-18 01:00:03 +0000

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 -------------------------------
---  __________ 
+--  __________
 -- < init.lua >
---  ---------- 
+--  ----------
 --    \
 --     \
 --        .--.
@@ -14,22 +14,24 @@
 --
 -------------------------------
 
-vim.o.wrap = false 
-vim.o.mouse = 'a' 
-vim.o.number = true 
-vim.o.backup = false 
-vim.o.swapfile = false 
-vim.o.encoding = 'utf-8' 
-vim.o.smartcase = true 
-vim.o.incsearch = true 
-vim.o.laststatus = 0 
-vim.o.shiftwidth = 4 
-vim.o.fileformat = 'unix' 
-vim.o.smartindent = true 
-vim.o.termguicolors = true 
-vim.g.onedarkhc_terminal_italics = 1 
+vim.o.wrap = false
+vim.o.mouse = 'a'
+vim.o.ruler = false
+vim.o.number = true
+vim.o.backup = false
+vim.o.swapfile = false
+vim.o.encoding = 'UTF-8'
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.laststatus = 0
+vim.o.shiftwidth = 4
+vim.o.fileformat = 'unix'
+vim.o.smartindent = true
+vim.o.termguicolors = true
+vim.g.onedarkhc_terminal_italics = 1
 
-vim.cmd 'set guicursor=' 
-vim.cmd 'colo onedarkhc' 
-vim.cmd 'autocmd BufWritePost *bashrc !source %' 
-vim.cmd 'autocmd BufWritePost *Xresources,*Xdefaults !xrdb %' 
+vim.cmd 'set cmdheight=1'
+vim.cmd 'set guicursor='
+vim.cmd 'colo onedarkhc'
+vim.cmd 'autocmd BufWritePost *bashrc !source %'
+vim.cmd 'autocmd BufWritePost *Xresources,*Xdefaults !xrdb %'
