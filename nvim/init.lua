6c030920c8fa84c58cf570d868ad452588499af0
xrft11 <85630585+xrft11@users.noreply.github.com>
2021-12-20 19:49:48 +0000

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--------------------------------
+-----------------------------------
 --  __________
 -- < init.lua >
 --  ----------
@@ -12,7 +12,7 @@
 --    /'\_   _/`\
 --    \___)=(___/
 --
--------------------------------
+-----------------------------------
 
 vim.o.wrap = false
 vim.o.mouse = 'a'
@@ -23,6 +23,8 @@ vim.o.swapfile = false
 vim.o.encoding = 'UTF-8'
 vim.o.smartcase = true
 vim.o.incsearch = true
+vim.o.guicursor = ''
+vim.o.cmdheight = 1
 vim.o.laststatus = 0
 vim.o.shiftwidth = 4
 vim.o.fileformat = 'unix'
@@ -30,8 +32,6 @@ vim.o.smartindent = true
 vim.o.termguicolors = true
 vim.g.onedarkhc_terminal_italics = 1
 
-vim.cmd 'set cmdheight=1'
-vim.cmd 'set guicursor='
 vim.cmd 'colo onedarkhc'
 vim.cmd 'autocmd BufWritePost *bashrc !source %'
 vim.cmd 'autocmd BufWritePost *Xresources,*Xdefaults !xrdb %'
