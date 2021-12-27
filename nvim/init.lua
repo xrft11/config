a13f7b5632c5161d932bfbb9e24a24362420abe1
xrft11 <85630585+xrft11@users.noreply.github.com>
2021-12-27 18:06:07 +0000

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,17 +14,24 @@
 --
 -----------------------------------
 
+vim.o.hls = false
 vim.o.wrap = false
+vim.g.t_Co = 256
 vim.o.mouse = 'a'
 vim.o.ruler = false
+vim.o.mouse = 'nicr'
 vim.o.number = true
 vim.o.backup = false
+vim.o.tabstop = 4
 vim.o.swapfile = false
 vim.o.encoding = 'UTF-8'
+vim.o.wildmenu = true
+vim.o.smarttab = true
 vim.o.smartcase = true
 vim.o.incsearch = true
 vim.o.guicursor = ''
 vim.o.cmdheight = 1
+vim.o.expandtab = true
 vim.o.laststatus = 0
 vim.o.shiftwidth = 4
 vim.o.fileformat = 'unix'
