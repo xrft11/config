a3171705a31a2fbbfd912642d92e8ac7f6da46aa
xrft11 <85630585+xrft11@users.noreply.github.com>
2022-01-21 13:40:39 +0000

    Update dwm.c

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1139,6 +1139,8 @@ manage(Window w, XWindowAttributes *wa)
 	configure(c); /* propagates border_width, if size doesn't change */
 	updatewindowtype(c);
 	updatesizehints(c);
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->x = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	updatewmhints(c);
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
