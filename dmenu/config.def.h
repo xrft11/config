9dbc7df8823ef84946c57f0bc96bacdef35ab7e6
xrft11 <85630585+xrft11@users.noreply.github.com>
2022-01-22 19:39:35 +0000

    Update config.def.h

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -6,7 +6,7 @@ static int centered = 1;        /* -c option; centers dmenu on screen           
 static int min_width = 800;     /* minimum width when centered                      */
 static const char *fonts[] = { "Roboto Mono:pixelsize=18:antialias=true:autohint=true" };
 static const char col_bg[] = "#21242b"; /* background */
-static const char col_fg[] = "#5c6370"; /* foreground */
+static const char col_fg[] = "#dfe5f2"; /* foreground */
 static const char sel_fg[] = "#dfe5f2"; /* selected foreground */
 static const char sel_bg[] = "#5c6370"; /* selected background*/
 static const char *prompt  = NULL;      /* -p  option; prompt to the left of input field */
@@ -14,9 +14,9 @@ static const char *colors[SchemeLast][2] = {
         /*              fg      bg      */
         [SchemeNor] = { col_fg, col_bg },
         [SchemeSel] = { sel_fg, sel_bg },
-        [SchemeBor] = { sel_fg, sel_fg },
-        [SchemeOut] = { sel_fg, sel_fg },
+        [SchemeBor] = { col_bg, col_fg },
 };
+
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines = 18;
 static unsigned int columns = 1;
